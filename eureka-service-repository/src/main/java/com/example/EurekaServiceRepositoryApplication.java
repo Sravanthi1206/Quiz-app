bab60c6931cbf692e208660a971a723143c93d0d
Sravanthi1206 <sravanthi.22bce20201@vitapstudent.ac.in>
2025-11-30 02:54:27 +0530

    updated

--- eureka-service-repository/src/main/java/com/example/EurekaServiceRepositoryApplication.java
+++ eureka-service-repository/src/main/java/com/example/EurekaServiceRepositoryApplication.java
@@ -2,12 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceRepositoryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceRepositoryApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServiceRepositoryApplication.class, args);
+  }
 }
